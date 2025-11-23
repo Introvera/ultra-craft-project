8aebdc72dad6711f549698953c518ad4797c1891
Malindu <malindu@zdatai.com>
2025-11-23 14:01:30 +0530

    feat: add ParallaxHero component to home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import { AboutUs } from '@/components/aboutus'
 import ContactUs from '@/components/contact'
 import Services from '@/components/services-component/services'
+import ParallaxHero from '@/components/hero_section'
 
 const page = () => {
   return (
     <>
+      <ParallaxHero/>
       <AboutUs />
       <Services />
       <ContactUs />
