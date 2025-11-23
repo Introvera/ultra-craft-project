9957422ad870618fc1462f3c6b97683d39695410
Kalahe Liyanage <deeshanaliyanage@gmail.com>
2025-11-23 13:14:07 +0530

    Add Services component and integrate into home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import { AboutUs } from '@/components/aboutus'
 import ContactUs from '@/components/contact'
+import Services from '@/components/services-component/services'
 
 const page = () => {
   return (
     <>
       <AboutUs />
+      <Services />
       <ContactUs />
     </>
       
